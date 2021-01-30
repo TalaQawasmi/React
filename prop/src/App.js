de1bb39afeca1b8bd6f85d754ce2e3c36d3fd9f7
TalaQawasmi <qawasmitala@gmail.com>
2021-01-30 18:30:41 +0200

    together

--- prop/src/App.js
+++ prop/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <NewComponent firstName={"Tala "}  LastName={"Qawasmi"} Age={25} HairColor={"Brown"}/>
-      <NewComponent firstName={"Diala"} LastName={"Qawasmi"} Age={22} HairColor={"Brown"}/>
+      <NewComponent firstName={"Diala"} LastName={"Qawasmi"} Age={22} HairColor={"Brown"} />
       <NewComponent firstName={"Lara"} LastName={"Qawasmi"} Age={19} HairColor={"Brown"}/>
       <NewComponent firstName={"Nana"} LastName={"Qawasmi"} Age={16} HairColor={"Brown"}/>
     </div>
--- prop/src/components/NewComponent.js
+++ prop/src/components/NewComponent.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
 
+
 class NewComponent extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            Age : this.props.Age,
+        }
+    }
+    birthday =()=>{
+        this.setState({Age : this.state.Age +1});
+    }
     render(){
-        const {firstName, LastName, Age, HairColor} = this.props;
+        const {firstName, LastName, HairColor} = this.props;
+        const {Age} = this.state;
         return(
             <div>
                 <h1>{firstName} {LastName}</h1>
                 <p>Age: {Age}</p>
                 <p>Hair Color: {HairColor}</p>
+                <button onClick={this.birthday}>Birthday Button for {firstName}{LastName}</button>
             </div>
         )
     }
+    
+    
 }
 export default NewComponent;
