cc60f828b2557f6b58948d0d9db6b480255a6491
TalaQawasmi <qawasmitala@gmail.com>
2021-01-31 15:21:27 +0200

    more-forms

---
+++ more-forms/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import MovieForm from './Components/MovieForm';
+
+function App() {
+  return (
+    <div className="app">
+      <MovieForm />
+    </div>
+    
+  );
+}
+
+export default App;
